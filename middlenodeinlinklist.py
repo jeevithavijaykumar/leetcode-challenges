59706c162e0df06c77a36549d1b1171d05624545
Jeevitha Vijayakumar <125430758+jeevithavijaykumar@users.noreply.github.com>
2023-06-12 17:26:36 -0700

    Create middlenodeinlinklist.py
    Solution to leetcode problem 876 - Middle node of the Linked List.

---
+++ middlenodeinlinklist.py
@@ -0,0 +1,12 @@
+# Given the head of a singly linked list, return the middle node of the linked list.
+# If there are two middle nodes, return the second middle node.
+
+class Middlenodelinkedlist():
+    def Middlenode(self, head):
+        slow = head
+        fast = head
+
+        while(fast and fast.next):
+            slow = slow.next
+            fast=fast.next.next
+        return(slow)
