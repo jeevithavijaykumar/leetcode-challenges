5cec7e6a6f11cacd3d26e8e6d67f3f784d7b04fb
Jeevitha Vijayakumar <125430758+jeevithavijaykumar@users.noreply.github.com>
2023-06-17 21:06:26 -0700

    Create validanagram.py
    Solution to leetcode problem 242 - Valid Anagram.

---
+++ validanagram.py
@@ -0,0 +1,24 @@
+#242. Valid Anagram
+#Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+#An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+# typically using all the original letters exactly once.
+
+class Anagram():
+    def isanagram(self,s,t):
+
+        if(len(s)!=len(t)):
+            return(False)
+        ds = {}
+        dt = {}
+
+        for i in range(0,len(s)):
+            ds[s[i]] = 1 + ds.get(s[i],0)
+            dt[t[i]] = 1 + dt.get(t[i],0)
+
+        for j in ds:
+            if(ds[j] != dt.get(j,0)):
+                return(False)
+        return(True)
+
+a=Anagram()
+print(a.isanagram("anagram","nagaram"))
