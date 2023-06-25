5f75a846691b9186e9eeec818dc60b3889fff2a6
Jeevitha Vijayakumar <125430758+jeevithavijaykumar@users.noreply.github.com>
2023-06-24 21:06:01 -0700

    Create maxproduct.py
    Solution to leetcode problem 152 - Maximum Product Subarray.

---
+++ maxproduct.py
@@ -0,0 +1,21 @@
+#152. Maximum Product Subarray
+#Given an integer array nums, find a subarray that has the largest product, and return the product.
+
+class Subarray():
+    def maxproduct(self,nums):
+        maxp = 1
+        minp= 1
+        res = max(nums)
+
+        for n in nums:
+            if(n==0):
+                maxp = 1
+                minp = 1
+            temp = maxp*n
+            maxp = max(temp, minp*n ,n)
+            minp = min(temp,minp*n,n)
+            res = max(res,maxp)
+        return(res)
+
+m = Subarray()
+print(m.maxproduct([2,-3,2,-4,-5]))
