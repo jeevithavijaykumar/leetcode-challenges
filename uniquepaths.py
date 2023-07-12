2383882de82937e442fa0f13652f7fdf823f8b78
Jeevitha Vijayakumar <125430758+jeevithavijaykumar@users.noreply.github.com>
2023-07-11 23:35:04 -0700

    Create uniquepaths.py
    Solution to leetcode problem 62 - Unique Paths.

---
+++ uniquepaths.py
@@ -0,0 +1,19 @@
+#62. Unique Paths
+#There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]).
+# The robot can only move either down or right at any point in time.
+#Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+#The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+class Grid:
+    def uniquepath(self,m,n):
+        row=[1]*n
+
+        for i in range(m-2,-1,-1):
+            newrow = [1]*n
+            for j in range(n-2,-1,-1):
+                newrow[j]=newrow[j+1]+row[j]
+            row=newrow
+        return(row[0])
+
+g = Grid()
+print(g.uniquepath(3,7))
