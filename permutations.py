e4f44eae5c93cdf0379f2cdd44634950138d4c4d
Jeevitha Vijayakumar <125430758+jeevithavijaykumar@users.noreply.github.com>
2023-10-25 15:28:48 -0700

    Create permutations.py
    Solution to leetcode problem 46 - Permutations.

---
+++ permutations.py
@@ -0,0 +1,25 @@
+#Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+#Input: nums = [1,2,3]
+#Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+class Permutations():
+    def permute(self,nums):
+        res=[]
+
+        if(len(nums)==1):
+            return([nums[:]])
+
+        for i in range(0,len(nums)):
+            n=nums.pop(0)
+            perms =self.permute(nums)
+            for perm in perms:
+                perm.append(n)
+            res.extend(perms)
+            nums.append(n)
+        return(res)
+
+p=Permutations()
+print(p.permute([1,2,3]))
+
+
+
