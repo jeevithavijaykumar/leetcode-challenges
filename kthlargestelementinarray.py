b3ad7d414b68ea195122fa04f9030811ff552f4a
Jeevitha Vijayakumar <125430758+jeevithavijaykumar@users.noreply.github.com>
2023-11-13 00:09:28 -0800

    Update kthlargestelementinarray.py
    Solution to Leetcode problem 215

--- kthlargestelementinarray.py
+++ kthlargestelementinarray.py
@@ -1,6 +1,5 @@
 #215. Kth Largest Element in an Array
 #Given an integer array nums and an integer k, return the kth largest element in the array.
-#Note that it is the kth largest element in the sorted order, not the kth distinct element.
 
 class Array():
     def kthlargest(self,nums,k):
@@ -10,17 +9,17 @@ class Array():
             p=l
             pivot=nums[r]
             for i in range(l,r):
-                if(nums[i]<=pivot):
-                    nums[i],nums[p]=nums[p],nums[i]
+                if(nums[i]<pivot):
+                    nums[p],nums[i]=nums[i],nums[p]
                     p=p+1
-            nums[p],nums[r]=nums[r],nums[p]
-            if(p>k):
-                return quickselect(l,p-1)
-            elif(p<k):
-                return quickselect(p+1,r)
+            nums[r],nums[p]=nums[p],nums[r]
+            if(k<p):
+                return(quickselect(l,p-1))
+            elif(k>p):
+                return(quickselect(p+1,r))
             else:
                 return(nums[p])
-        return quickselect(0,len(nums)-1)
+        return(quickselect(0,len(nums)-1))
 
 k=Array()
 print(k.kthlargest([3,2,3,1,2,4,5,5,6],4))
