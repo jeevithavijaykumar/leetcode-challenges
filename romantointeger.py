5d3557c633ef1ab2986c1eb755bafeeee7d103a0
Jeevitha Vijayakumar <125430758+jeevithavijaykumar@users.noreply.github.com>
2023-11-27 21:03:30 -0800

    Create romantointeger.py
    Solution to Leetcode problem 13 - Roman to Integer.

---
+++ romantointeger.py
@@ -0,0 +1,16 @@
+#13. Roman to Integer
+#Given a roman numeral, convert it to an integer.
+class String():
+    def romantointeger(self,s):
+        rti = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        res = 0
+
+        for i in range(0, len(s)):
+            if (i + 1 < len(s) and rti[s[i]] < rti[s[i + 1]]):
+                res = res - rti[s[i]]
+            else:
+                res = res + rti[s[i]]
+        return (res)
+
+s=String()
+print(s.romantointeger('MCMXCIV'))
