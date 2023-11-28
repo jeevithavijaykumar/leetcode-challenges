446fb3e02369cded9426edf20338f311a8a37cf7
Jeevitha Vijayakumar <125430758+jeevithavijaykumar@users.noreply.github.com>
2023-11-27 21:05:07 -0800

    Update romantointeger.py
    Comments added.

--- romantointeger.py
+++ romantointeger.py
@@ -1,5 +1,13 @@
 #13. Roman to Integer
 #Given a roman numeral, convert it to an integer.
+# Symbol       Value
+# I             1
+# V             5
+# X             10
+# L             50
+# C             100
+# D             500
+# M             1000
 class String():
     def romantointeger(self,s):
         rti = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
