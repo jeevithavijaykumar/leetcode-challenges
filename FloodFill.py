beb4f688be8ea2bcc092b9b49dbc787bede27eb7
Jeevitha Vijayakumar <125430758+jeevithavijaykumar@users.noreply.github.com>
2023-11-29 16:33:59 -0800

    Update FloodFill.py
    Added Solution2 - Recursively.

--- FloodFill.py
+++ FloodFill.py
@@ -4,6 +4,7 @@
 #You should perform a flood fill on the image starting from the pixel image[sr][sc].
 #Return the modified image after performing the flood fill.
 
+#Approach1 - Using Queues
 import collections
 class Matrix():
     def floodfill(self,image,sr,sc,color):
@@ -27,6 +28,30 @@ class Matrix():
                     image[r][c] = color
                     q.append((r, c))
         return (image)
+
+m=Matrix()
+print(m.floodfill([[1,1,1],[1,1,0],[1,0,1]],1,1,2))
+
+#Approach2 - Recursively
+class Matrix():
+    def floodfill(self,image,sr,sc,color):
+        rows=len(image)
+        cols=len(image[0])
+        start_color=image[sr][sc]
+
+        def dfs(row,col,image):
+            if(row not in range(rows) or col not in range(cols)):
+                return
+            if(image[row][col]==color or image[row][col]!=start_color):
+                return
+            image[row][col]=color
+            directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+            for dr, dc in directions:
+                r=row+dr
+                c=col+dc
+                dfs(r,c,image)
+        dfs(sr,sc,image)
+        return(image)
 
 m=Matrix()
 print(m.floodfill([[1,1,1],[1,1,0],[1,0,1]],1,1,2))
