fb279545a0604fb5fcb8d6bb08b2b98d2ee0ce68
Jeevitha Vijayakumar <125430758+jeevithavijaykumar@users.noreply.github.com>
2023-12-02 23:57:41 -0800

    Update combinationsum.py
    Solution to leetcode problem 39 - Combination sum.

--- combinationsum.py
+++ combinationsum.py
@@ -1,7 +1,6 @@
 #39. Combination Sum
 #Given an array of distinct integers candidates and a target integer target,
-# return a list of all unique combinations of candidates where the chosen numbers sum to target.
-# You may return the combinations in any order.
+#Return a list of all unique combinations of candidates where the chosen numbers sum to target.
 #The same number may be chosen from candidates an unlimited number of times.
 
 class Integers():
