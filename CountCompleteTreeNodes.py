46bb0791d54af75f40f29f599491ab2db39e06c6
Jeevitha Vijayakumar <125430758+jeevithavijaykumar@users.noreply.github.com>
2023-12-18 21:30:29 -0800

    Create CountCompleteTreeNodes.py
    Solution to Leetcode problem 222 - Number of nodes in a binary tree.

---
+++ CountCompleteTreeNodes.py
@@ -0,0 +1,25 @@
+#Given the root of a complete binary tree, return the number of the nodes in the tree.
+
+class TreeNode():
+    def __init__(self,val):
+        self.val=val
+        self.left=None
+        self.right=None
+
+class BinaryTree():
+    def countNodes(self,root):
+
+        def dfs(node):
+            if(not node):
+                return(0)
+            return(dfs(node.left)+dfs(node.right)+1)
+
+        return(dfs(root))
+
+root=TreeNode(1)
+root.left=TreeNode(2)
+root.right=TreeNode(3)
+root.left.right=TreeNode(4)
+b=BinaryTree()
+print(b.countNodes(root))
+
