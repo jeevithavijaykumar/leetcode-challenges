33507e189220c73d105074816c0c7268d79ce628
Jeevitha Vijayakumar <125430758+jeevithavijaykumar@users.noreply.github.com>
2024-01-17 20:58:29 -0800

    Create 3SumClosest.py
    Solution to leetcode problem 16 - three integers in nums such that the sum is closest to target.

---
+++ 3SumClosest.py
@@ -0,0 +1,23 @@
+#Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+#Return the sum of the three integers.
+
+class Solution():
+    def threesumclosest(self,nums,target):
+        nums.sort()
+        res=sum(nums[:3])
+
+        for i in range(0,len(nums)-2):
+            l=i+1
+            r=len(nums)-1
+            while(l<r):
+                total=nums[i]+nums[l]+nums[r]
+                if(abs(total-target)<abs(res-target)):
+                    res=total
+                if(total<target):
+                    l=l+1
+                else:
+                    r=r-1
+        return(res)
+
+s=Solution()
+print(s.threesumclosest([-1,2,1,-4],1))
