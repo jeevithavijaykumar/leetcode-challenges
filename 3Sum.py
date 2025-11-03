825d71b72a35c8af0a47d2a79388a17e153b9695
Jeevitha Vijayakumar <jeevithavijaydr@gmail.com>
2025-11-02 20:14:13 -0800

    Update 3Sum.py

--- 3Sum.py
+++ 3Sum.py
@@ -31,6 +31,8 @@ class Solution:
                 h = h-1
                 while(l<h and nums[l] == nums[l-1]):
                     l = l+1
+                while (l < h and nums[h] == nums[h -1]):
+                    h = h - 1
 
 
 s = Solution()
