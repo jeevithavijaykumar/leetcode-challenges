28873b8a2a6013691421cccaf56738286b7a064d
Jeevitha Vijayakumar <jeevithavijaydr@gmail.com>
2025-11-04 23:23:39 -0800

    Create singleNumber.py

---
+++ singleNumber.py
@@ -0,0 +1,15 @@
+# 136. Single Number
+# Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+# You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+class Solution:
+    def singleNumber(self, nums):
+        duplicate_list=[]
+
+        for num in nums:
+            if num not in duplicate_list:
+                duplicate_list.append(num)
+            else:
+                duplicate_list.remove(num)
+
+        return duplicate_list.pop()
